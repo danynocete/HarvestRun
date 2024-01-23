960905a8eb5cb7f5d1d13ae9f6d67e981d9d8dad
Dany Nocete <nocete.dany@gmail.com>
2024-01-23 18:36:30 +1300

    Added hearts to show Player's remaining lives.Updated life logic

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -7,6 +7,11 @@ public class PlayerLife : MonoBehaviour
 {
     private Animator playerAnimator;
     private Rigidbody2D playerRigidBody;
+
+    private int heartIndex = 2;
+
+    [SerializeField] private List<GameObject> hearts; //array of Heart Objects
+
     void Start()
     {
         playerAnimator = GetComponent<Animator>();
@@ -18,8 +23,26 @@ public class PlayerLife : MonoBehaviour
      */
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //if player collides with Fly object or the ground they should die
-        if (collision.gameObject.CompareTag("Fly") || collision.gameObject.CompareTag("Ground")) 
+        //if player collides with Fly object or the thorns they should and lose a life
+        if (collision.gameObject.CompareTag("Fly")|| collision.gameObject.CompareTag("Trap"))
+        {
+            //if current lives > 0 restart level
+            if(hearts.Count > 0)
+            {
+                Destroy(hearts[heartIndex]);
+                hearts.RemoveAt(heartIndex);
+                heartIndex = heartIndex - 1;
+                Debug.Log("hearts list: " + hearts.Count);
+            }
+            //if current lives <0 game ends
+            if(hearts.Count < 1)
+            {
+                PlayerDies();
+                //load Game Over scene
+            }
+        }
+        //if Player falls to the ground they instantly die
+        if (collision.gameObject.CompareTag("Ground"))
         {
             PlayerDies();
         }
@@ -40,5 +63,6 @@ public class PlayerLife : MonoBehaviour
     private void RestartLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name); //load scene that the user is on
+   
     }
 }
