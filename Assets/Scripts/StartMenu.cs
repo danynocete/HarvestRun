2710eb3aa1385da07fc018d6e827e5ba7d76cab9
Dany Nocete <nocete.dany@gmail.com>
2024-01-28 11:42:45 +1300

    Added endgame scene

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -10,6 +10,6 @@ public class StartMenu : MonoBehaviour
      */
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //load level 1
+        SceneManager.LoadScene(1); //load level 1
     }
 }
